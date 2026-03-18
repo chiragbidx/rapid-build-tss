0f5ba813794812e4a4514f46cc2e4de0bc41f82e
chiragbidx <chirag@bidx.ai>
2026-03-18 09:38:20 +0530

    Annotate subject type in contact section

--- components/home/LayoutContactSection.tsx
+++ components/home/LayoutContactSection.tsx
@@ -190,7 +190,7 @@ export const LayoutContactSection = () => {
                             </SelectTrigger>
                           </FormControl>
                           <SelectContent>
-                            {contact.formSubjects.map((subject) => (
+                            {contact.formSubjects.map((subject: string) => (
                               <SelectItem key={subject} value={subject}>
                                 {subject}
                               </SelectItem>
